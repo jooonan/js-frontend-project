57f59672bb798192f02018f30edb3ca719b18bc1
Johan An <johanan9606@gmail.com>
2024-11-01 16:20:03 +0100

    added dark mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,33 @@
 import React from 'react'
 import Logotype from '../assets/images/silicon.svg'
+import DarkModeSwitch from './DarkModeSwitch'
 
 const Navbar = () => {
+
   return (
     <section className="navbar">
 
       <div className="container">
           
-          <a className="silicon-logo" href="index.html">
-              <img src={Logotype} />
-              <span>Silicon</span>
-          </a>
-          
-          <nav className="navbar">
-              <a className="nav-link" href="#">Features</a>
-              <a className="nav-link" href="contact.html">Contact</a>
-          </nav>
-          
-          
-          <div className="btn-toggle-switch">
-              <span className="label">Dark mode</span>
-              <label htmlFor="darkmode-switch" className="toggle-switch">
-                  <input type="checkbox" id="darkmode-switch" />
-                  <span className="slider round"></span>
-              </label>
-          </div>
-          
-          <a className="btn-primary" href="#">
-              <i className="fa-regular fa-user"></i>
-              <span>Sign in / up</span>
-          </a>
+        <a className="silicon-logo" href="index.html">
+            <img src={Logotype} />
+            <span>Silicon</span>
+        </a>
+        
+        <nav className="navbar">
+            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link" href="contact.html">Contact</a>
+        </nav>
+        
+        <DarkModeSwitch />
+        
+        <a className="btn-primary" href="#">
+            <i className="fa-regular fa-user"></i>
+            <span>Sign in / up</span>
+        </a>
           
       </div>
-  </section>
+    </section>
   )
 }
 
