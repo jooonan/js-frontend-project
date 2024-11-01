cde9a5866ea8ced9a44fba3093b71f686ddd3f47
Johan An <johanan9606@gmail.com>
2024-11-01 14:49:20 +0100

    added header and footer sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './assets/css/main.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div className="wrapper">
+      <Header />
+      <main>
+
+      </main>
+      <Footer />
+    </div>
   )
 }
 export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+          <p>© 2024 Silicon. All rights reserved. Credit MadrasThemes</p>
+      </div>
+  </footer>
+  )
+}
+
+export default Footer
