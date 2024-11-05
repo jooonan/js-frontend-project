181738d3e87f5a74a28108106a4dd03419a97148
Johan An <johanan9606@gmail.com>
2024-11-06 00:45:26 +0100

    added api for faq section

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,16 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import FaqItem from './FaqItem'
 
 const FaqSection = () => {
 
-    const [faqItems, setFaqItems] = useState([
-        { id: 1, title: "Is any of my personal information stored in the App?", content: "No, the app does not store any personal information. All data is encrypted and stored securely on our servers. You have full control over your data at all times." },
-        { id: 2, title: "What formats can I download my transaction history in?", content: "You can download your transaction history in various formats such as PDF, CSV, and Excel. This allows you to easily manage and review your transactions." },
-        { id: 3, title: "Can I schedule future transfers?", content: "Yes, you can schedule future transfers. Simply choose the date and time for the transfer and the app will handle the rest, ensuring that your transfer is processed on time." },
-        { id: 4, title: "When can I use Banking App services?", content: "Our banking services are available 24/7, so you can access your account and make transactions anytime, from anywhere." },
-        { id: 5, title: "Can I create my own password that is easy for me to remember?", content: "Yes, you can create your own password following our security guidelines. We recommend using a combination of letters, numbers, and special characters for a strong and memorable password." },
-        { id: 6, title: "What happens if I forget or lose my password?", content: "If you forget your password, you can reset it using the 'Forgot Password' feature. A verification code will be sent to your registered email or phone number to help you reset your password." },
-    ])
+    const [faqItems, setFaqItems] = useState([])
+
+    const fetchFaq = async () => {
+        const res = await fetch('https://win24-assignment.azurewebsites.net/api/faq')
+        const data = await res.json()
+        setFaqItems(data)
+    }
+
+    useEffect(() => {
+        fetchFaq()
+    }, [])
 
 
   return (
