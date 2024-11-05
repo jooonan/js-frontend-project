a9c7509bfeca5d605fa2ec19fa4e69facba47b4e
Johan An <johanan9606@gmail.com>
2024-11-06 00:30:47 +0100

    code for opening and closing faq item and added id

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const FaqItem = ({item}) => {
+    const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <div className="faq-card">
-        <div className="question">
-            <h2>{item.title}</h2>
-            
-            <button className="btn-question">
-                <i className="fa-solid fa-chevron-down"></i>
-            </button>
+    const handleClick = () => {
+      setIsOpen(prev => !prev);
+    };
+  
+    return (
+      <div className={`faq-card ${isOpen ? 'open' : ''}`}>
+        <div className="question" onClick={handleClick}>
+          <h2>{item.title}</h2>
+          <button className="btn-question">
+            <i className={`fa-solid ${isOpen ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
+          </button>
         </div>
-
-        <div className="answer">
+          <div className="answer">
             <div className="expandable">
-                <p>{item.content}</p>
+              <p>{item.content}</p>
             </div>
-        </div>
-    </div>
+          </div>
+      </div>
   )
 }
 
